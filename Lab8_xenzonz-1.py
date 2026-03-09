5acdea8c851ccb35a5b069f3161a76e7b88bad5e
sam cocquyt <samcocquyt04@gmail.com>
2026-03-08 22:36:44 -0400

    intro docstring added

--- Lab8_xenzonz-1.py
+++ Lab8_xenzonz-1.py
@@ -1,1 +1,13 @@
 print("hello world")
+
+"""
+Docstring for Lab8_xenzonz_1
+i. LAB 8: UPC Validator
+ii. Sam Cocquyt
+iii.  Program that validates a 12-digit UPC-A code. 
+    The program will ask the user for a 12-digit UPC, use a function to calculate the correct check digit, and then inform the user if the full UPC is valid.
+    Has input validation. Before calling the find_UPC() function, check that the user's input is exactly 12 characters long and contains only numbers. 
+    If the input is invalid, print an error and ask again.
+iv. No starter code
+v. 3/8/2026
+"""
